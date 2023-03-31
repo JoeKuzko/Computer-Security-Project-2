42b7702d6f31f54368ff281c82bc03e7f06f1759
JoeKuzko <90813383+JoeKuzko@users.noreply.github.com>
2023-03-31 16:46:32 -0500

    modified:   header.h

--- header.h
+++ header.h
@@ -13,11 +13,12 @@ public:
     string Password; // will be recieved as input though user input
     string Salt; // should be 12 to 48 bits long
     string SaltedPassword; // salt is added to password
+    string SaltedPaddedPassword; // string to hold final string after padding
 
     void GenerateSalt(); // will random generate a salt
     void AddSaltToPassword(); // self-explanetory
-    void AddPaddingToSaltedPassword(); // padding is up to length of new string modulo 512 = 64;
-    // next step padding??
+    void AddPaddingToSaltedPassword(); // padding is up to length of new string modulo 512 bits = 64 bits
+    void AddOriginalLengthOfMessageAsPadding(); // the 64 bits are padded on as the original password length
 
 
 
