0d0d4758c3369c1c62ca8be1c6a1108e84854f2f
JoeKuzko <90813383+JoeKuzko@users.noreply.github.com>
2023-03-31 16:39:16 -0500

    new file:   MD5.cpp 	new file:   header.h 	new file:   info md5.txt 	new file:   main.cpp

---
+++ header.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+class MD5{
+public:
+    string Password; // will be recieved as input though user input
+    string Salt; // should be 12 to 48 bits long
+    string SaltedPassword; // salt is added to password
+
+    void GenerateSalt(); // will random generate a salt
+    void AddSaltToPassword(); // self-explanetory
+    // next step padding??
+
+
+
+};
