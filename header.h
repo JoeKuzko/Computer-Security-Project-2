fe3add5378d9e8fe1d58fe7e123fcb3052e6cff0
JoeKuzko <90813383+JoeKuzko@users.noreply.github.com>
2023-03-31 19:23:34 -0500

    https://github.com/JoeKuzko/Computer-Security-Project-2.git

--- header.h
+++ header.h
@@ -9,17 +9,19 @@
 using namespace std;
 
 class MD5{
+private:
+    static string Password; // will be recieved as input though user input
+    static string Salt; // should be 12 to 48 bits long
+    static string SaltedPassword; // salt is added to password
+    static string SaltedPaddedPassword; // string to hold final string after padding
+
+    static void GetPassword(); // gets user inputed password
+    static void GenerateSalt(); // will random generate a salt
+    static void AddSaltToPassword(); // self-explanetory
+    static void AddPaddingToSaltedPassword(); // padding is up to length of new string modulo 512 bits = 64 bits
+    static void AddOriginalLengthOfMessageAsPadding(); // the 64 bits are padded on as the original password length
 public:
-    string Password; // will be recieved as input though user input
-    string Salt; // should be 12 to 48 bits long
-    string SaltedPassword; // salt is added to password
-    string SaltedPaddedPassword; // string to hold final string after padding
-
-    void GenerateSalt(); // will random generate a salt
-    void AddSaltToPassword(); // self-explanetory
-    void AddPaddingToSaltedPassword(); // padding is up to length of new string modulo 512 bits = 64 bits
-    void AddOriginalLengthOfMessageAsPadding(); // the 64 bits are padded on as the original password length
-
+    void Hash(string x); // hashes the final string after padding????
 
 
 };
