acf023e79275b83f3ba1d01a12cd6b1e968d51b0
JoeKuzko <90813383+JoeKuzko@users.noreply.github.com>
2023-04-03 01:20:03 -0500

    modified:   MD5.cpp 	modified:   a.exe 	modified:   header.h 	modified:   info md5.txt

--- MD5.cpp
+++ MD5.cpp
@@ -1,5 +1,73 @@
 #include "header.h"
 
 string MD5::Hash(string input){
-    return input;
+    string padded;
+    string finalOutput;
+    int* messageArray;
+    padded = AddPaddingTo(input);
+    messageArray = SubBlocks(padded);
+
+    return finalOutput;
 }
+
+string MD5::AddPaddingTo(string input ){
+    long long int originalBitSize =0;
+    string binaryOfInput;
+    originalBitSize = (input.length()*8);
+    bitset<8> bits;
+    for (char c : input) {
+        bits = bitset<8>(c);
+        binaryOfInput = binaryOfInput + bits.to_string();
+    }
+    //cout << (448 - originalBitSize) <<"\n";
+    binaryOfInput = binaryOfInput + '1'; // padding is always required
+
+    if (( binaryOfInput.length() % 448) != 0){
+        int paddingNeeded = 448 - ( binaryOfInput.length() % 448);
+        //cout << paddingNeeded <<"\n";
+        for(int i = 0; i < paddingNeeded ;i++){
+            binaryOfInput = binaryOfInput + '0';
+        }
+    }
+    // now to add the original length of the input string as padding
+    bitset<64> orgStringLength;
+    orgStringLength = bitset<64>(originalBitSize);
+    binaryOfInput = binaryOfInput + orgStringLength.to_string();
+
+    //cout << orgStringLength;
+    //cout <<binaryOfInput << "\n";
+
+    return binaryOfInput;
+}
+
+int* MD5::SubBlocks(string block){ // 512 bit as string input only
+    static int messageArray[16];
+    string bucket = "";
+    int j = 0;
+    for(int i = 0;i < block.length();i++ ){
+        bucket = bucket + block[i];
+        if ((bucket.length() % 32) == 0){
+            int temp = 0;
+            temp = BitStringToInt(bucket);
+            messageArray[j] = temp;
+            j++;
+            bucket = "";
+        }
+    }
+    cout << messageArray[15];
+    return messageArray;
+}
+
+int MD5::BitStringToInt(string subBlock){
+    int block = 0;  //32 bit int for sublock
+    int j = 31;
+    for (int i = 0; i < 32; i++){
+        if(subBlock[i] == '1'){
+            block = block + pow(2,j);
+        }
+        j--;
+    }
+    //cout << block << "\n";
+    return block;
+}
+
--- header.h
+++ header.h
@@ -3,8 +3,12 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cstdint>
+#include <cmath>
 #include <stdlib.h>
 #include <time.h>
+#include <bitset>
+#include <vector>
 
 using namespace std;
 
@@ -12,10 +16,23 @@ using namespace std;
 
 class MD5{
 private:
-    //static void AddPaddingTo(); // padding is up to length of new string modulo 512 bits = 64 bits
-    //static void AddOriginalLengthOfMessageAsPadding(); // the 64 bits are padded on as the original password length
+    static const unsigned int WordA = 19088743;     // 19088743 equivalent to 0x01234567
+    static const unsigned int WordB = 2309737967;   // 2309737967 equivalent to 0x89abcdef
+    static const unsigned int WordC = 4275878552;   // 4275878552 equivalent to 0xfedcba98
+    static const unsigned int WordD = 1985229328;   // 1985229328 equivalent to 0x76543210
+
+    static string AddPaddingTo(string); // padding is added to make string a multiple of 512 bits. returns string of binary representation of the original inputted string
+    static int* SubBlocks(string);      // recieves message as 512 bits is further broken down to 16 sub blocks of 32 bits into integers;
+    static int BitStringToInt(string);  // converts string of binary to integer to hold 32 bits
+
+
+
+    /*
+        still need to make sure that teh string is plit into 512 bit blocks
+        need to make functions for step 3+
+    */
 public:
-    static string Hash(string x); // recieves string as input and returns final hash
+    static string Hash(string);         // recieves string as input and returns final hash
     
 
 };
